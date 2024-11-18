f1fcd0452f6bcea7d0b5564db353f5f583e4ec1f
Tonis Joosep <tonisjoosep@gmail.com>
2024-11-18 23:23:52 +0200

    Fix pep 8 errors in views.py, add temporary logic to access options without category selected

--- game/urls.py
+++ game/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("game/", views.game, name="game"),
+    path('options/<int:category_id>/', views.options, name='options'),
 ]
--- game/views.py
+++ game/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
 
+
 def game(request):
     return HttpResponse("Welcome to the game")
+
 
 def index(request):
     return render(request, "index.html")
 
-def options(request, category_id):
-    return render(request, "options.html")
+
+def options(request, category_id=None):
+    if category_id:
+        return render(request, "options.html", {'category_id': category_id})
+    else:
+        return render(request, 'options.html', {'message': 'No category selected.'})
+
 
 def login(request):
     return render(request, "login.html")
-
-
