c6248493d6795cf50cd5118a5d1f50e11ef84e3e
Tonis Joosep <tonisjoosep@gmail.com>
2024-11-23 17:42:26 +0200

    remove duplicate paths for options and register

--- game/urls.py
+++ game/urls.py
@@ -5,7 +5,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.index, name='index'),
     path("game/", views.game, name="game"),
-    path('options/<int:category_id>/', views.options, name='options'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.register_view, name='register'),
