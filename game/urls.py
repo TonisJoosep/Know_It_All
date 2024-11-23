11fca96a4d615955ac5616b528b0d1290edc2687
Tonis Joosep <tonisjoosep@gmail.com>
2024-11-23 11:38:35 +0200

    Show logged in user on main page, delete duplicate paths

--- game/urls.py
+++ game/urls.py
@@ -3,10 +3,12 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path("game/", views.game, name="game"),
     path('options/<int:category_id>/', views.options, name='options'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.register_view, name='register'),
     path('forgot_password', views.forgot_password, name='forgot_password'),
+
 ]
